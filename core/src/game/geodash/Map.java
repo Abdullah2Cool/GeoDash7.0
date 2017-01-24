b48b23de1e58a16a59a35e1aa267c31a2c7d36d0
Abdullah2Cool <abdullahbinasad14@gmail.com>
2017-01-24 13:49:28 -0500

    Everything works.

--- core/src/game/geodash/Map.java
+++ core/src/game/geodash/Map.java
@@ -19,7 +19,6 @@ public class Map {
     private OrthogonalTiledMapRenderer tiledMapRenderer;
     private TiledMap map;
     private World world;
-//    Spikes spikes;
 
     public Map(String sPath, World world, boolean bObstacles, boolean bPortals, GamGeoDash game) {
         map = new TmxMapLoader().load(sPath);
@@ -40,35 +39,6 @@ public class Map {
         tiledMapRenderer.setView(camera);
         tiledMapRenderer.render();
     }
-
-//    private void loadObstacles(MapObjects obstacles) {
-//        for (MapObject obstacle : obstacles) {
-//            Shape shape;
-////            if (obstacle instanceof PolylineMapObject) {
-//            shape = createPolyLine((PolylineMapObject) obstacle);
-////            } else {
-////                continue;
-////            }
-//            Body body;
-//            BodyDef def = new BodyDef();
-//            def.type = BodyDef.BodyType.StaticBody;
-//            body = world.createBody(def);
-//            body.createFixture(shape, 1.0f);
-//            shape.dispose();
-//        }
-//    }
-//
-//    private ChainShape createPolyLine(PolylineMapObject obstacle) {
-//        float[] vertices = obstacle.getPolyline().getTransformedVertices();
-//        Vector2[] worldVertices = new Vector2[vertices.length / 2];
-//
-//        for (int i = 0; i < worldVertices.length; i++) {
-//            worldVertices[i] = new Vector2(vertices[i * 2] / PPM, vertices[i * 2 + 1] / PPM);
-//        }
-//        ChainShape chainShape = new ChainShape();
-//        chainShape.createChain(worldVertices);
-//        return chainShape;
-//    }
 
     public TiledMap getMap() {
         return map;
